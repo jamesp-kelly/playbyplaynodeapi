c2fed3b47da46dadd3e3b6326ca0ee7da2909f2b
James Kelly <jk_jameskelly@outlook.com>
2017-03-27 10:36:50 -0400

    use mongo

---
+++ catRoutes.js
@@ -0,0 +1,76 @@
+var _ = require('lodash');
+var Cat = require('./catModel');
+
+module.exports = function(app) {
+
+
+  //create
+  app.post('/cat', function(req, res) {
+    var newCat = new Cat(req.body);
+    newCat.save(function(err) {
+      if (err) {
+        res.json({info: 'error during cat create', error: error});
+      }
+      res.json({info: 'cat created successfully'});
+    });
+  });
+
+  //read
+  app.get('/cat', function(req, res) {
+    Cat.find(function(err, cats) {
+      if (err) {
+        res.json({info: 'error during cat create', error: error});
+      } else {
+        res.json({info: 'cat found successfully', data: cats});
+      }
+    });
+  });
+
+  //read
+  app.get('/cat/:id', function(req, res) {
+    Cat.findById(req.params.id, function(err, cat) {
+      if (err) {
+        res.json({info: 'error during find cat', error: err});
+      }
+      if (cat) {
+        res.json({info: 'cat found successfully', data: cat});
+      } else {
+        res.json({info: 'cat not found'});
+      }
+    });
+  });
+
+  //update
+  app.put('/cat/:id', function(req, res) {
+    Cat.findById(req.params.id, function(err, cat) {
+      if (err) {
+        res.json({info: 'error during find cat', error: err});
+      } else {
+        if (cat) {
+          _.merge(cat, req.body);
+          cat.save(function(err) {
+            if (err) {
+              res.json({info: 'error during cat update', error: err});
+            } else {
+              res.json({info: 'cat updated successfully'});
+            }
+          });
+        } else {
+          res.json({info: 'cat not found'});
+        }
+      }
+    });
+  });
+
+  //delete
+  app.delete('/cat/:id', function(req, res) {
+    Cat.findByIdAndRemove(req.params.id, function(err) {
+      if (err) {
+        res.json({info: 'error during remove cat', error: err});
+      } else {
+        res.json({info: 'cat removed successfully'});
+      }
+    });
+  });
+
+}
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-var cats = require('./cats')(app);
+var catRoutes = require('./catRoutes')(app);
 
 var server = app.listen(3000, function() {
   console.log('server running on port 3000');
